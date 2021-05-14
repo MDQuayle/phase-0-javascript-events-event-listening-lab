339be793e758b189df8e18e0fc3c715b44a85511
mdquayle <misternopants@gmail.com>
2021-05-14 18:09:39 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+function clickAlert() {
+  alert('What a click!');
 }
+
+function addingEventListener() {
+    const input = document.getElementById('input');
+input.addEventListener('click', clickAlert);
+}
+
+input.addEventListener('click', clickAlert);
